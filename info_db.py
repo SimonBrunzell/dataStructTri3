b19293aca2dcdc439c21cb82bf52af43b2293f7a
simon <simoncbrunzell@gmail.com>
2022-03-15 17:00:12 -0700

    The sub lists are now being printed with all three forms of iteration

--- info_db.py
+++ info_db.py
@@ -17,7 +17,7 @@ list_of_PII.append({
     "DOB": "March 7th 2006",
     "Residence": "San Diego",
     "Email": "Sebastian@gmail.com",
-    "Classes": ["A", "B", "C", "D", "E"]
+    "Classes": ["Class_1", "Class_2", "Class_3", "Class_4", "Class_5", ]
 })
 
 
@@ -26,12 +26,72 @@ first = list_of_PII[1].get("FirstName")
 last = list_of_PII[1].get("LastName")
 full_name = first + " " + last
 
-# asking what their Period 1 class
-A = input(full_name + " what is your first class: ")
+print(full_name + " is this your class list?")
 
-# trying to input the users response into the sublist
+
+# prints list backwards
+# def class_loop():
+#     n = len(list_of_PII[1]["Classes"]) - 1
+#     while n > -1:
+#         print(list_of_PII[1]["Classes"][n])
+#         n = n - 1
+# class_loop()
+
+
+# prints all of Sebastian's original classes
+def class_loop_while():
+    n = 0
+    while n < 5:
+        print(list_of_PII[1]["Classes"][n])
+        n = n + 1
+
+
+class_loop_while()
+
+
+# # prints all of sebastian's classes as the list
+# print(list_of_PII[1]["Classes"])
+
+# asks for a correction of Sebastian's Period 1 Class
+print()
+A = input("sorry somethings wrong... what's your first class: ")
+
+# imputing period 1 into class sublist
 list_of_PII[1]["Classes"][0] = A
 
 print()
-print("Your Period 1 is: ")
-print(list_of_PII[1].get("Classes")[0])
+print(" oh... this is your class list")
+print()
+# using a for loop to display revamped schedule
+for classes in list_of_PII[1]["Classes"]:
+    print(classes)
+
+print()
+print("are you sure your second class is right")
+B = input("what is your 2nd period class?: ")
+print()
+
+list_of_PII[1]["Classes"][1] = B
+
+
+def recursive_loop(c):
+    if c < len(list_of_PII[1]["Classes"]):
+        print(list_of_PII[1]["Classes"][c])
+        recursive_loop(c + 1)
+    return
+
+
+recursive_loop(0)
+
+print()
+print("Ooh there we go... this looks right now")
+
+
+# n = 0
+# while n < 5:
+#     print(list_of_PII[1]["Classes"][n])
+#     n = n + 1
+
+
+
+
