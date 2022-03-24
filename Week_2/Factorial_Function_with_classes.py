9154d12db30cfe9a546afc65eb13fc5370348580
simon <simoncbrunzell@gmail.com>
2022-03-23 21:01:45 -0700

    I created a program that will return the factorial of an input utilizing classes

--- Week_2/Factorial_Function_with_classes.py
+++ Week_2/Factorial_Function_with_classes.py
@@ -1,3 +1,30 @@
 
+# The Assignment:
 # Write Factorial function using classes, providing implementation of call.
+
+import math
+#
+# print("the factorial of 23 is: ")
+# print(math.factorial(23))
+
+
+class Factorial:
+    def __init__(self, terms):
+        self.terms = int(terms)
+
+    def print_fact(self):
+        fact = math.factorial(self.terms)
+        return fact
+
+
+response = (input("What do you want the Factorial of? : "))
+n = Factorial(response)
+Factorial.terms = n
+
+
+print(Factorial.print_fact(Factorial.terms))
+
+
+
+
 
