df4dd179046691aab9bd30a06803dfd9c958de7e
simon <simoncbrunzell@gmail.com>
2022-03-24 18:38:25 -0700

    created a working palindrome test, utilizing classes

--- Week_2/Palindrome_Function_with_Classes.py
+++ Week_2/Palindrome_Function_with_Classes.py
@@ -11,41 +11,44 @@ class Palindrome:
         # self.word = str(self.word)
         # return self.word
         # do a while loop using len()
-        x = len(self.word)
-        while x > 0:
-            if self.word[len(self.word)-(x-1)] == self.word[len(self.word)]:
-                x = x - 1
-                if x == 0:
-                    answer = "yes"
-                    return answer
+
+        n = len(self.word)
+        m = 1
+        while n > 0:
+            if self.word[len(self.word) - n] == self.word[len(self.word) - m]:
+                n = n - 1
+                m = m + 1
             else:
-                answer = "no"
-                return answer
+                n = -10
+
+        if n == 0:
+            # print(self.word + " is a palindrome")
+            answer = "yes that's a palindrome"
+            return answer
+        else:
+            answer = " No that's not a palindrome"
+            return answer
 
 
-# response = (input("What word do you want to check: "))
-# n = Palindrome(response)
-# Palindrome.word = n
+response = (input("What word do you want to check: "))
+n = Palindrome(response)
+Palindrome.word = n
+
+print("{}" .format(Palindrome.pal_check(Palindrome.word)))
+
+
+# # Working Palindrome Test
+# word = input("what word do you want to test?: ")
+# n = len(word)
+# m = 1
+# while n > 0:
+#     if word[(len(word) - n)] == word[len(word) - m]:
+#         n = n - 1
+#         m = m + 1
+#     else:
+#         n = -10
+#         print(word + " is not a palindrome")
 #
-# print(Palindrome.pal_check(Palindrome.word))
-
-
-# Working Palindrome Test
-word = input("what word do you want to test?: ")
-n = len(word)
-m = 1
-while n > 0:
-    if word[(len(word) - n)] == word[len(word) - m]:
-        n = n - 1
-        m = m + 1
-
-if n == 0:
-    print(word + " is a palindrome")
-else:
-    print(word + " is not a palindrome")
-
-
-
-
-
-
+#
+# if n == 0:
+#     print(word + " is a palindrome")
