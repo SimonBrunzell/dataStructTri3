acd98ba744427e2b6b64da41dd9c48d65cc82ed4
SanjayB06 <sanjaymbharadwaj@gmail.com>
2022-03-29 14:12:50 -0700

    animation in menu w/ color

--- menu.py
+++ menu.py
@@ -1,5 +1,7 @@
 from Week_0.ani import ship
 from Week_0.animated_fish import yes_to_chase
+import os
+import time
 mainmenu = {
     1: {
         "Display": "Ship",
@@ -21,8 +23,20 @@ mainmenu = {
 def buildMenu(menu):
     for key,item in menu.items():
         print(f"{key} : {item['Display']}")
+def printmenubar(n,display=None):
+    os.system("clear")
+    print("\033[34m="*n)
+    if display:
+        print("="," "*int((n-5-len(display))/2),f"{display}"," "*int((n-5-len(display))/2),"=")
+    else:
+        print("="," "*(n-4),"=")
+    print("="*n,"\033[0m")
 
 def presentMenu(menu):
+    for x in range(49):
+        printmenubar(x)
+        time.sleep(0.01)
+    printmenubar(50,"Python Main Menu")
     buildMenu(menu)
     choice = int(input("Which item do you want? "))
     while choice not in menu:
